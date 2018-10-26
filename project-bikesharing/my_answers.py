69a3551c4e95561c79ef5ef9337eb4143a41a0aa
Bjarten <bms_mok@hotmail.com>
2018-10-26 16:05:10 +0200

    feat: implement sigmoid activation function

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -16,11 +16,11 @@ class NeuralNetwork(object):
                                        (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
         
-        #### TODO: Set self.activation_function to your implemented sigmoid function ####
+        #### Set self.activation_function to your implemented sigmoid function ####
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x : 1/(1+np.exp(-x))  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
