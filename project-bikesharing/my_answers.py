936ec5deb7faf623f515a76d30161636fa2c55e6
Bjarten <bms_mok@hotmail.com>
2018-10-26 16:20:39 +0200

    feat: implement forward pass

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -63,13 +63,14 @@ class NeuralNetwork(object):
         '''
         #### Implement the forward pass here ####
         ### Forward pass ###
-        # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        # Hidden layer - Replace these values with your calculations.
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
 
-        # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
+
+        # Output layer - Replace these values with your calculations.
+        final_inputs = np.dot(hidden_outputs, weights_hidden_to_output)# signals into final output layer
+        final_outputs = self.activation_function(final_inputs) # signals from final output layer
         
         return final_outputs, hidden_outputs
 
