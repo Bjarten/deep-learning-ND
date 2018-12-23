c1e7e9fef63ac43b11eff2ddac9a24f29f5e3f59
Bjarten <bms_mok@hotmail.com>
2018-12-23 11:45:44 +0100

    fix:(p3): remove code from previous project

--- project-tv-script-generation/helper.py
+++ project-tv-script-generation/helper.py
@@ -22,9 +22,6 @@ def preprocess_and_save_data(dataset_path, token_lookup, create_lookup_tables, v
     """
     text = load_data(dataset_path)
     
-    # Ignore notice, since we don't use it for analysing the data
-    #text = text[81:]
-
     token_dict = token_lookup()
     for key, token in token_dict.items():
         text = text.replace(key, ' {} '.format(token))
